c107cb1de2a3a21e0faeb075faecf46c67dcd436
N00rAhmed <tronn232003@gmail.com>
2023-11-05 12:33:56 +0000

    added id

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 
 //get all tasks
 const getTasks = async (req, res) => {
-    const tasks = await Task.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+
+    const tasks = await Task.find({ user_id }).sort({createdAt: -1})
 
     res.status(200).json(tasks)
 }
@@ -43,7 +45,8 @@ const createTask = async(req, res) => {
 
     // doc to db
     try{
-        const task = await Task.create({title, description})
+        const user_id = req.user._id
+        const task = await Task.create({title, description, user_id})
         res.status(200).json(task)
     }catch (error) {
         res.status(400).json({error: error.message})
--- models/TaskModel.js
+++ models/TaskModel.js
@@ -10,6 +10,10 @@ const taskSchema = new Schema({
     description: {
         type: String,
         required: true
+    },
+    user_id: {
+        type: String,
+        required: true
     }
 }, { timestamps: true })
 
